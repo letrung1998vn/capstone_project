59d6e4f242faacbfcdabbb3042c1d45409bff9cf
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-19 00:55:04 +0700

    update

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -4,12 +4,7 @@ CLUSTER="hello"
 REGION="us-east-1"
 CLUSTER_NAME="${CLUSTER}.${REGION}.eksctl.io"
 
-if ./bin/kubectl config get-clusters | grep -q ${CLUSTER_NAME}; then
-    echo
-    echo "Cluster '${CLUSTER_NAME}' already exists!"
-    echo
-else
-    echo
-    echo "Creating cluster..."
-    ./bin/eksctl create cluster -f hello_cluster.yml
-fi
+echo
+echo "Creating cluster..."
+./bin/eksctl create cluster -f hello_cluster.yml
+
