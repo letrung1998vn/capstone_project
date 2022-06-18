5028092619d319085269ce545f53876055167550
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-19 01:00:43 +0700

    Update eks_create_cluster.sh

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-CLUSTER="hello"
-REGION="us-east-1"
-CLUSTER_NAME="${CLUSTER}.${REGION}.eksctl.io"
-
 echo
 echo "Creating cluster..."
 ./bin/eksctl create cluster -f hello_cluster.yml
