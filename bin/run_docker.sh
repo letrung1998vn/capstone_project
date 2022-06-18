4723fb348f8b14914731c395884e6e3b9246087b
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-18 19:25:44 +0700

    update

--- bin/run_docker.sh
+++ bin/run_docker.sh
@@ -4,7 +4,5 @@ CONTAINER_NAME="letrung1998vn/hello-app"
 VERSION=1.000
 CONTAINER_PORT=80
 HOST_PORT=80
-DOCKER_USER="letrung1998vn"
-DOCKER_PWD="socnhi.com1234"
 # Run flask app
 docker run -p ${HOST_PORT}:${CONTAINER_PORT} ${CONTAINER_NAME}:${VERSION}
