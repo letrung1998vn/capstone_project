e8ba592991b4f92bb5880d024a21a01c0cd919d5
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-18 19:03:43 +0700

    update

--- bin/run_docker.sh
+++ bin/run_docker.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-CONTAINER_NAME="gampie/hello-app"
+CONTAINER_NAME="letrung1998vn/hello-app"
 VERSION=1.000
 CONTAINER_PORT=80
 HOST_PORT=80
-
+DOCKER_USER="letrung1998vn"
+DOCKER_PWD="socnhi.com1234"
+# Login Docker
+docker login -u ${DOCKER_USER} -p ${DOCKER_PWD}
 # Run flask app
 docker run -t --rm -p ${HOST_PORT}:${CONTAINER_PORT} ${CONTAINER_NAME}:${VERSION}
