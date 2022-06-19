d13fe05b3fdd615887ec378003cd9f49ea56c3c8
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-19 12:37:16 +0700

    update

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -3,5 +3,6 @@
 echo
 echo "Creating cluster..."
 aws configure import --csv  file://new_user_credentials.csv
+aws configure set region us-east-1 --profile new_user_credentials
 ./bin/eksctl create cluster --config-file=hello_cluster.yml
 
