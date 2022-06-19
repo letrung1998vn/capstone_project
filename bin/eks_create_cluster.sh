cb275e0134813f38caaf5f11bec4ee07a280adbc
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-19 14:06:11 +0700

    update

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -3,7 +3,10 @@
 echo
 echo "Creating cluster..."
 aws configure import --csv  file://new_user_credentials.csv
-aws configure set region us-east-1 --profile deploy-user
+echo "get indentity"
+aws configure list
+aws configure list-profiles
+aws configure
 aws sts get-caller-identity
 ./bin/eksctl create cluster -f hello_cluster.yml
 
