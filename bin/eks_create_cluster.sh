66d2f6f52330253845863cf7ddf47be2356a178e
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-19 14:31:24 +0700

    Update eks_create_cluster.sh

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -5,7 +5,6 @@ echo "Creating cluster..."
 aws configure set aws_access_key_id AKIAV54HP5HIPYWRQDEO
 aws configure set aws_secret_access_key 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
 aws configure set region us-east-1
-aws configure set default.ca_bundle /udapeople.pem
 aws sts get-caller-identity
 ./bin/eksctl create cluster -f hello_cluster.yml
 
