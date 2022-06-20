29241fe5bf8e1481d0c965ae634faf184ff4dc67
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-20 14:18:08 +0700

    Update k8s_deployment.sh

--- bin/k8s_deployment.sh
+++ bin/k8s_deployment.sh
@@ -11,12 +11,20 @@ VERSION=1.000
 
 dockerpath=${DOCKER_HUB_ID}/${DOCKER_REPOSITORY}:${VERSION}
 
+# List kubernetes resources
+echo
+echo "Listing deployments"
+./bin/kubectl get deployments -o wide
+echo
+echo "Listing services"
+./bin/kubectl get services -o wide
+echo
+echo "Listing pods"
+./bin/kubectl get pods -o wide
+
 # Run the Docker Hub container with kubernetes
 ./bin/kubectl create deployment ${DEPLOYMENT_NAME} --image=${dockerpath}
-./bin/kubectl config get-contexts
-./bin/kubectl config use-context docker-desktop
-# See the status
-kubectl get deploy,rs,svc,pods
+
 ./bin/kubectl expose deployment/${DEPLOYMENT_NAME} --type="LoadBalancer" --port ${CONTAINER_PORT}
 
 # List kubernetes resources
