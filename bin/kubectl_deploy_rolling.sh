1fef2e1da3208a4b1db8e59c97d1c1fa935d7fd9
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-21 23:24:34 +0700

    update

---
+++ bin/kubectl_deploy_rolling.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Set vars
+DOCKER_HUB_ID="letrung1998vn"
+DOCKER_REPOSITORY="hello-world-app"
+DEPLOYMENT_NAME=${DOCKER_REPOSITORY}
+CONTAINER_PORT=80
+VERSION=lastest
+dockerpath=${DOCKER_HUB_ID}/${DOCKER_REPOSITORY}:${VERSION}
+
+# set aws configure
+aws configure set aws_access_key_id AKIASPJPHRDZWOII2ZFL
+aws configure set aws_secret_access_key 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
+aws configure set region us-east-1
+aws sts get-caller-identity
+# Create config file
+echo "Create config"
+aws eks --region us-east-1 update-kubeconfig --name capstone-cluster
+kubectl get svc
+# Run the Docker Hub container with kubernetes
+echo "Create deployment"
+kubectl edit deployment ${DEPLOYMENT_NAME}
+
+# List kubernetes resources
+echo
+echo "Listing deployments"
+kubectl get deployments -o wide --all-namespaces
+echo
+echo "Listing services"
+kubectl get services -o wide --all-namespaces
+echo
+echo "Listing pods"
+kubectl get pods -o wide --all-namespaces
+kubectl get nodes -o wide --all-namespaces
+
--- bin/rollout_status.sh
+++ bin/rollout_status.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 #Vars
-DEPLOYMENT_NAME=hello-world-app
-
+DEPLOYMENT_NAME="hello-world-app"
+DOCKER_HUB_ID="letrung1998vn"
+DOCKER_REPOSITORY="hello-world-app"
+DEPLOYMENT_NAME=${DOCKER_REPOSITORY}
+VERSION=lastest
+dockerpath=${DOCKER_HUB_ID}/${DOCKER_REPOSITORY}:${VERSION}
 # get kube config
 aws eks --region us-east-1 update-kubeconfig --name capstone-cluster
 kubectl get svc
 
 # get rollout status of cluster
+kubectl set image ${DEPLOYMENT_NAME}=${dockerpath}
 kubectl rollout status deployment ${DEPLOYMENT_NAME}
 echo
 kubectl get deployments -o wide
