1bded08a2245715913a0d159a6d7f5e80c64e3a3
letrung1998vn <40650762+letrung1998vn@users.noreply.github.com>
2022-06-22 23:33:59 +0700

    update

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -6,7 +6,5 @@ aws configure set aws_access_key_id AKIASPJPHRDZW7MGKX3C
 aws configure set aws_secret_access_key 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
 aws configure set region us-east-1
 aws sts get-caller-identity
-aws eks --region us-east-1 update-kubeconfig --name capstone-cluster
-kubectl get svc
 ./bin/eksctl create cluster -f cluster.yml
 
